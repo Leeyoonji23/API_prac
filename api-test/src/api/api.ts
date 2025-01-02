e4c59c3ac5f6bd28809638c4a803f42e4611c838
Yoonji Lee <rhaxlddbswl3@gmail.com>
2025-01-02 21:32:44 +0900

    feat: api

--- api-test/src/api/api.ts
+++ api-test/src/api/api.ts
@@ -7,7 +7,34 @@ const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
 
+//글 목록
 export const getPosts = async () => {
   const response = await axiosInstance.get("/posts");
+  return response.data;
+};
+
+//글 조회
+export const getPostId = async (postId: number) => {
+  const response = await axiosInstance.get(`/posts/${postId}`);
+  return response.data;
+};
+
+export const createPost = async (postData: { title: string; body: string }) => {
+  const response = await axiosInstance.post("/posts", postData);
+  return response.data;
+};
+
+//게시글 수정
+export const updatePost = async (
+  postId: number,
+  postData: { title: string; body: string }
+) => {
+  const response = await axiosInstance.put(`/posts/${postId}`, postData);
+  return response.data;
+};
+
+//게시글 삭제
+export const deletePost = async (postId: number) => {
+  const response = await axiosInstance.delete(`/posts/${postId}`);
   return response.data;
 };
---
+++ api-test/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import PostList from "../components/PostList";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>블로그</h1>
+      <PostList />
+    </div>
+  );
+};
+
+export default Home;
